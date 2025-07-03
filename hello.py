e9f6b33acb2db94d9b8c997b325062529b76228e
Alexander Antonov <vikniksor@gmail.com>
2025-07-03 21:02:46 +0300

    Basic strings

--- hello.py
+++ hello.py
@@ -1,1 +1,20 @@
-print("Hello, world")
+#Ask user for their name, strip and title
+name = input("What's your name? ").strip().title()
+
+#Remove whitespace from string
+# name = name.strip()
+
+#Capitalize title() & strip whitespace
+# name = name.strip().title()
+
+
+#Split user's name into first and last names
+
+firstname, lastname = name.split(" ")
+
+#Say hello to user
+# print("Hello, " + name)
+# print("Hello,", name, sep=" - ", end="\n\n")
+# print("Hello,", name)
+
+print(f"Hello, {firstname}")
